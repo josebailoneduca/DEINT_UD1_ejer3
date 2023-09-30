51a55c7efda867793eaced477a320eb2c9a03503
Jose Javier Bailón Ortiz <josebailon@gmail.com>
2023-09-30 05:20:49 +0200

    Inicio

---
+++ src/ud1_ejer3/Cliente.java
@@ -0,0 +1,16 @@
+/*
+LICENCIA JOSE JAVIER BO
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+Lista de paquetes:
+ */
+
+package ud1_ejer3;
+
+/**
+ *
+ * @author Jose Javier Bailón Ortiz
+ */
+class Cliente {
+
+}//end Cliente
